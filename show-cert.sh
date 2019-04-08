9591fd76b835ad4c24fe8aaa5cabd007412c9c0f
Dan Higham <dan.higham@gmail.com>
2019-04-08 09:47:56 -0700

    Updated README and removed RC in favor of DEP

--- show-cert.sh
+++ show-cert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-podname=$(kubectl get po -o json | jq -r '.items[0].metadata.name')
+podname=$(kubectl get po -o json | jq -r '.items[2].metadata.name')
 kubectl exec -it $podname -- cat /home/mitmproxy/.mitmproxy/mitmproxy-ca-cert.cer
